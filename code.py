1a6548ae3620ea16e8370bf2f3a0aa07c0d45439
venkat8306 <svkmarvelous@gmail.com>
2024-11-20 21:38:00 +0530

    "Add comments to explain function"

--- code.py
+++ code.py
@@ -39,6 +39,7 @@
 """Code for repo: Oil_spill_Detector. This code handles core operations."""
 """Code for repo: Oil_spill_Detector. This code handles core operations."""
 """Code for repo: Oil_spill_Detector. This code handles core operations."""
+"""Code for repo: Oil_spill_Detector. This code handles core operations."""
 import torch
 from transformers import MllamaForConditionalGeneration, AutoProcessor
 import math
